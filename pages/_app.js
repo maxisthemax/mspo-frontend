d8b111a416e74696bfdb7ed5ccbddd6fc8e925a4
maxisthemax <maxisthemax89@gmail.com>
2022-02-28 11:23:22 +0800

    init

--- pages/_app.js
+++ pages/_app.js
@@ -34,7 +34,7 @@ export default function MyApp(props) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>Gladax</title>
+        <title>MYEZGM</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
--- useSwr/useTicket.js
+++ useSwr/useTicket.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import axios from "utils/http-anxios";
 import useSwrHttp from "./useSwrHttp";
 import useUser from "./useUser";
 
 export default function useTicket() {
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(25);
+  const [sort, setSort] = useState();
+
   const { data, mutate, error, isValidating } = useSwrHttp("tickets", {
-    sort: ["createdAt:desc"],
+    pagination: { page, pageSize },
+    sort: sort,
   });
 
   const { userData } = useUser();
@@ -16,7 +22,12 @@ export default function useTicket() {
         createdBy: userData.id,
       },
     });
+    resetTicketSort();
     mutate();
+  };
+
+  const resetTicketSort = () => {
+    setSort(["createdAt:desc"]);
   };
 
   return {
@@ -25,5 +36,12 @@ export default function useTicket() {
     error,
     isValidating,
     addTicket,
+    ticketPage: page,
+    setTicketPage: setPage,
+    ticketPageSize: pageSize,
+    setTicketPageSize: setPageSize,
+    ticketSort: sort,
+    setTicketSort: setSort,
+    resetTicketSort,
   };
 }
