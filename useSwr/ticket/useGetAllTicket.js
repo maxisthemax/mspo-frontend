378c458b771fd9bc0e73cbbb7e57ad75a8d5e788
maxisthemax <maxisthemax89@gmail.com>
2022-03-10 09:30:12 +0800

    fix the useSwr function

--- useSwr/ticket/useGetAllTicket.js
+++ useSwr/ticket/useGetAllTicket.js
@@ -1,41 +1,76 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "utils/http-anxios";
 import useSwrHttp from "useSwr/useSwrHttp";
 import useUser from "useSwr/user/useUser";
+import { useSnackbar } from "notistack";
 
 export default function useGetAllTicket() {
+  //*define
+  const { enqueueSnackbar } = useSnackbar();
+  const { userData } = useUser();
+  const companyId = userData?.company?.id;
+
+  //*useState
   const [page, setPage] = useState(0);
   const [pageSize, setPageSize] = useState(25);
   const [sort, setSort] = useState(["createdAt:desc"]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const { data, mutate, error, isValidating } = useSwrHttp("tickets", {
-    pagination: { page, pageSize },
-    sort: sort,
-    populate: "*",
-  });
-
-  const { userData } = useUser();
+  const { data, mutate, error, isValidating } = useSwrHttp(
+    companyId ? "tickets" : null,
+    {
+      filters: {
+        company: {
+          id: {
+            $eq: companyId || "",
+          },
+        },
+      },
+      pagination: { page, pageSize },
+      sort: sort,
+      populate: ["transporter"],
+    }
+  );
 
   const addTicket = async ({ ticketNo }) => {
-    await axios.post("tickets", {
-      data: {
-        ticket_no: ticketNo,
-        createdBy: userData.id,
-      },
-    });
-    resetTicketSort();
-    mutate();
+    setIsLoading(true);
+    try {
+      await axios.post("tickets", {
+        data: {
+          ticket_no: ticketNo,
+          createdBy: userData.id,
+        },
+      });
+      enqueueSnackbar(`Ticket ${ticketNo} Added Success`, {
+        variant: "success",
+      });
+      resetTicketSort();
+      mutate();
+    } catch (error) {
+      if (error?.response?.data?.error?.message)
+        enqueueSnackbar(error?.response?.data?.error?.message, {
+          variant: "error",
+        });
+    }
+    setIsLoading(false);
   };
 
   const resetTicketSort = () => {
     setSort(["createdAt:desc"]);
   };
 
+  useEffect(() => {
+    if (error?.response?.data?.error?.message)
+      enqueueSnackbar(error?.response?.data?.error?.message, {
+        variant: "error",
+      });
+  }, [error]);
+
   return {
     ticketData: data,
-    mutate,
-    error,
-    isValidating,
+    mutateTicketData: mutate,
+    ticketDataIsValidating: isValidating,
+    ticketDataIsLoading: isLoading,
     addTicket,
     ticketPage: page,
     setTicketPage: setPage,
--- useSwr/transporter/useGetSingleTransporter.js
+++ useSwr/transporter/useGetSingleTransporter.js
@@ -1,23 +1,22 @@
 import { useEffect, useState } from "react";
-import axios from "utils/http-anxios";
 import useSwrHttp from "useSwr/useSwrHttp";
+import axios from "utils/http-anxios";
 import useUser from "useSwr/user/useUser";
+import useGetAllTransporter from "./useGetAllTransporter";
 import { useSnackbar } from "notistack";
 
-export default function useGetAllTransporter() {
+export default function useGetSingleTransporter(id) {
   //*define
   const { enqueueSnackbar } = useSnackbar();
   const { userData } = useUser();
   const companyId = userData.company.id;
+  const { mutateAllTransporterData } = useGetAllTransporter();
 
   //*useState
-  const [page, setPage] = useState(0);
-  const [pageSize, setPageSize] = useState(25);
-  const [sort, setSort] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
   const { data, mutate, error, isValidating } = useSwrHttp(
-    companyId ? "transporters" : null,
+    id && companyId ? `transporters/${id}` : null,
     {
       filters: {
         company: {
@@ -26,30 +25,19 @@ export default function useGetAllTransporter() {
           },
         },
       },
-      pagination: { page, pageSize },
-      sort: sort,
-      populate: ["ticket"],
     }
   );
 
-  const resetAllTransporterSort = () => {
-    setSort();
-  };
-
-  const addSingleTransporter = async ({ name }) => {
+  const editSingleTransporter = async ({ name }) => {
     setIsLoading(true);
     try {
-      await axios.post("transporters", {
+      await axios.put(`transporters/${id}`, {
         data: {
           name: name,
-          createdBy: userData.id,
         },
       });
-      enqueueSnackbar(`Transporter ${name} Added Success`, {
-        variant: "success",
-      });
-      resetAllTransporterSort();
       mutate();
+      mutateAllTransporterData();
     } catch (error) {
       if (error?.response?.data?.error?.message)
         enqueueSnackbar(error?.response?.data?.error?.message, {
@@ -67,17 +55,10 @@ export default function useGetAllTransporter() {
   }, [error]);
 
   return {
-    allTransporterData: data,
-    mutateAllTransporterData: mutate,
-    addSingleTransporter,
-    allTransporterDataIsValidating: isValidating,
-    allTransporterDataIsLoading: isLoading,
-    allTransporterPage: page,
-    setAllTransporterPage: setPage,
-    allTransporterPageSize: pageSize,
-    setAllTransporterPageSize: setPageSize,
-    allTransporterSort: sort,
-    setAllTransporterSort: setSort,
-    resetAllTransporterSort,
+    singleTransporterData: data,
+    mutateSingleTransporterData: mutate,
+    singleTransporterDataIsValidating: isValidating,
+    singleTransporterDataIsLoading: isLoading,
+    editSingleTransporter,
   };
 }
